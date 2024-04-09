da6fb67dbc44fcc045f25fef7b7b8908a37771f2
pradipta-behera <pradiptabehera117@gmail.com>
2024-04-09 11:19:58 +0530

    changes in home

--- client/src/pages/CreateQuiz.jsx
+++ client/src/pages/CreateQuiz.jsx
@@ -9,44 +9,47 @@ function CreateQuiz() {
   return (
 
     <div className='CreateQuiz'>
-        <div className='createquiz-navbar'>
-        <AfterNav/>
-        </div>
-        <div className='createquiz-container'>
-      <div className='createquiz-content'>
-        <div className='createquiz-ques'>
-          <div className="input-container">
-            <input type="text" placeholder="Enter your question" id="enter-ques" name="enter-ques" />
+      <div className='createquiz-navbar'>
+        <AfterNav />
+      </div>
+      <div className='createquiz-container'>
+        <div className='createquiz-content'>
+          <div className='createquiz-ques'>
+            <div className="input-container">
+              <input type="text" placeholder="Enter your question" id="enter-ques" name="enter-ques" />
+            </div>
+          </div>
+          <div className='createquiz-options'>
+            <div className="input-container">
+              <input type="text" placeholder="Enter your answer" id="enter-answer1" name="enter-answer" />
+              <TiTick className="tick-icon" />
+              <ImBin className="bin-icon" />
+            </div>
+            <div className="input-container">
+              <input type="text" placeholder="Enter your answer" id="enter-answer2" name="enter-answer" />
+              <TiTick className="tick-icon" />
+              <ImBin className="bin-icon" />
+            </div>
+            <div className="input-container">
+              <input type="text" placeholder="Enter your answer" id="enter-answer3" name="enter-answer" />
+              <TiTick className="tick-icon" />
+              <ImBin className="bin-icon" />
+            </div>
+            <div className="input-container">
+              <input type="text" placeholder="Enter your answer" id="enter-answer4" name="enter-answer" />
+              <TiTick className="tick-icon" />
+              <ImBin className="bin-icon" />
+            </div>
           </div>
         </div>
-        <div className='createquiz-options'>
-          <div className="input-container">
-            <input type="text" placeholder="Enter your answer" id="enter-answer1" name="enter-answer" />
-            <TiTick className="tick-icon" />
-            <ImBin className="bin-icon" />
-          </div>
-          <div className="input-container">
-            <input type="text" placeholder="Enter your answer" id="enter-answer2" name="enter-answer"/>
-            <TiTick className="tick-icon" />
-            <ImBin className="bin-icon" />
-          </div>
-          <div className="input-container">
-            <input type="text" placeholder="Enter your answer" id="enter-answer3" name="enter-answer"/>
-            <TiTick className="tick-icon" />
-            <ImBin className="bin-icon" />
-          </div>
-          <div className="input-container">
-            <input type="text" placeholder="Enter your answer" id="enter-answer4" name="enter-answer"/>
-            <TiTick className="tick-icon" />
-            <ImBin className="bin-icon" />
-          </div>
+        <div className='createquiz-button'>
+          <button type='save'>Save Question</button>
         </div>
       </div>
-    </div>
-        <div className='createquiz-footer'>
-        <Footer/>
-        </div>
-      
+      <div className='createquiz-footer'>
+        <Footer />
+      </div>
+
     </div>
   )
 }
